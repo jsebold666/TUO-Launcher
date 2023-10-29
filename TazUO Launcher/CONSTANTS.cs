b243bca420ff7c1330c22a0112843878efb228a1
Tad Taylor <bittiez@gmail.com>
2023-10-28 22:57:42 -0600

    Initial commit

---
+++ TazUO Launcher/CONSTANTS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TazUO_Launcher
+{
+    class Constants
+    {
+        public class COLORS
+        {
+            public static Color DARK_GRAY = Color.FromArgb(255, 34, 40, 49);
+            public static Color MEDIUM_GRAY = Color.FromArgb(255, 57, 62, 70);
+            public static Color ORANGE = Color.FromArgb(244, 214, 90, 49);
+            public static Color DARK_WHITE = Color.FromArgb(244, 238, 238, 238);
+        }
+    }
+}
---
+++ TazUO Launcher/LauncherSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TazUO_Launcher
+{
+    class LauncherSettings
+    {
+        public static Version LauncherVersion
+        {
+            get
+            {
+                Version version = Assembly.GetExecutingAssembly().GetName().Version;
+                return version == null ? new Version(1, 0, 0) : version;
+            }
+        }
+    }
+}
