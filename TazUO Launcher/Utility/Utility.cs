7fdf6c43f803fbaf39f6822e4f40e8cf859fc407
Tad Taylor <bittiez@gmail.com>
2023-12-06 21:37:49 -0700

    Add import data folder option

--- TazUO Launcher/Utility/Utility.cs
+++ TazUO Launcher/Utility/Utility.cs
@@ -43,7 +43,21 @@ namespace TazUO_Launcher.Utility
             }
             else
             {
-                return "";
+                return string.Empty;
+            }
+        }
+
+        public static string AskForFolder()
+        {
+            Ookii.Dialogs.Wpf.VistaFolderBrowserDialog folderBrowserDialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == true)
+            {
+                return folderBrowserDialog.SelectedPath;
+            } 
+            else
+            {
+                return string.Empty;
             }
         }
 
@@ -55,6 +69,44 @@ namespace TazUO_Launcher.Utility
                 UseShellExecute = true,
             };
             System.Diagnostics.Process.Start(sInfo);
+        }
+
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs = true)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
         }
     }
 }
